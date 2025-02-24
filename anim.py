3a40b495a457cb22907c0844d83339bd8023aaf5
roshanleharwani <roshanleharwani@gmail.com>
2025-02-24 21:58:38 +0530

    Quality set to low for preview

--- anim.py
+++ anim.py
@@ -5,29 +5,35 @@ import subprocess
 
 
 def generate_animation_video(animation_json: str):
-  animation = json.loads(animation_json)
-  code = animation["code"]
-  scenes = animation["scenes"]
-  title = animation["title"]
+    animation = json.loads(animation_json)
+    code = animation["code"]
+    scenes = animation["scenes"]
+    title = animation["title"]
 
-  output = f"output/{title}"
-  filename = title.lower()
-  filepath = f"{output}/{filename}.py"
+    output = f"output/{title}"
+    filename = title.lower()
+    filepath = f"{output}/{filename}.py"
 
-  if not os.path.exists(output):
-    os.makedirs(output)
+    if not os.path.exists(output):
+        os.makedirs(output)
 
-  with open(filepath, "w") as file:
-    file.write(code)
+    with open(filepath, "w") as file:
+        file.write(code)
 
-  for scene in scenes:
-    command = ["manim", f"{filename}.py", scene, "-qh", "-o", "video"]
+    for scene in scenes:
+        command = ["manim", f"{filename}.py", scene, "-ql", "-o", "video"]
 
-    try:
-      result = subprocess.run(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, text=True, cwd=output)
-      if result.stderr:
-        print(f"[{filepath}:{scene}]: result.stderr")
-    except Exception:
-      print("Error running manim")
+        try:
+            result = subprocess.run(
+                command,
+                stdout=subprocess.PIPE,
+                stdin=subprocess.PIPE,
+                text=True,
+                cwd=output,
+            )
+            if result.stderr:
+                print(f"[{filepath}:{scene}]: result.stderr")
+        except Exception:
+            print("Error running manim")
 
-  return f"{output}/media/videos/{filename}/1080p60/video.mp4"
+    return f"{output}/media/videos/{filename}/1080p60/video.mp4"
